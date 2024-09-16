b67877bacf53fbf2a07f6b8ab1c8ec94cdf636a0
Jackson L. Bryant <145219269+JacksonBryantFGCU@users.noreply.github.com>
2024-09-16 08:29:19 -0400

    Update InfoText.jsx

--- src/pages/Home/InfoText.jsx
+++ src/pages/Home/InfoText.jsx
@@ -1,12 +1,8 @@
-// src/components/InfoText.jsx
 const InfoText = () => {
     return (
         <div className="bg-gray-100 py-16 px-6" id="InfoText">
             <div className="max-w-4xl mx-auto text-center">
                 <h1 className="text-4xl font-bold text-gray-800 mb-6">Steps to Join</h1>
-                <p className="text-lg mb-6">
-                    Welcome everyone! As we organize and do our best to grow our Pickleball community here at Sunstone at Wellen Park, we are making sure we have access to platforms that other various clubs/communities have, so that we can share, grow, learn, and come together with the best virtual and physical experience possible. 
-                </p>
                 <h1 className="text-2xl mb-5">Join Sunstone Pickleball by doing the following: </h1>
                 <p className="text-lg mb-8">
                     1.) Fill out the Sunstone Pickleball Club Signup Form: <span className="font-semibold text-blue-600"><a href="https://forms.gle/z7u7AxwRcnabhZch8" target="_blank">Click Here</a></span>
@@ -36,6 +32,9 @@ const InfoText = () => {
                     </a>
                 </div>
                 <p className="text-lg pt-5">TeamReach Code: Sunstone123</p>
+                <p className="text-lg mb-8">
+                    4.) Check the calendar for play times: <span className="font-semibold text-blue-600"><a href="/calendar" >Click Here</a></span>
+                </p>
             </div>
         </div>
     );
